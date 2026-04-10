8519d76aefc330839a7b7762eb2ef2d6ffb37c0e
Graeme <grmbrn89@gmail.com>
2026-04-10 16:46:11 +0200

    add google sign in

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -9,6 +10,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
   ],
   pages: {
     signIn: '/login',
